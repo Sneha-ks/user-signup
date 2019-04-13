2d56fcdb2147e6b02ed5cf0147fdd3c84df96156
Sneha KS <snehaa.ks@gmail.com>
2019-04-12 22:02:16 -0400

    Added logic to validate input

--- main.py
+++ main.py
@@ -5,6 +5,30 @@ app.config['DEBUG'] = True
 
 @app.route('/')
 def index():
-    return render_template("welcome.html")
+    return render_template("welcome.html",title="SignUp")
+
+@app.route('/signup', methods=['POST'])
+def signup():
+    username = request.form['username']
+    password = request.form['password']
+    password1 = request.form['password1']
+    email = request.form['email']
+
+    usernameError = ""
+    passwordError = ""
+    password1Error = ""
+    emailError = ""
+
+    if not username:
+        usernameError = "Username can't be empty"
+    else:
+        if (len(username) < 3) and (len(username)> 20):
+            usernameError = "Username should be between 3 and 20 characters in length"
+        #else:
+    #else
+    if not password:
+        passwordError = "Password can't be empty"
+    #else 
+
 
 app.run()
